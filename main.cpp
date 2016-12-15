ef87c116318566d199d783e069b5272278e96555
Muriam <d_muriam@inbox.ru>
2016-12-15 22:52:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <cstring>
 #include <locale>
-#include "find.h"//файл хранящий прототипы(find.h)
+#include "find.h"//С„Р°Р№Р» С…СЂР°РЅСЏС‰РёР№ РїСЂРѕС‚РѕС‚РёРїС‹(find.h)
 
 using namespace std;
 
-main(){
+main(){    
 	FILE *text;
 	char buff[50], index;
 	while(1){
 		system("cls");
 		cout << "***Irregular Verbs***" << endl;
 		cout << "Enter the verb: ";
-		cin.getline(buff,50);//ввод слова и запись его в char buff
-		for(int i(0); i < 50; i++){ // посимвольный перевод в нижний регистр
+		cin.getline(buff,50);//РІРІРѕРґ СЃР»РѕРІР° Рё Р·Р°РїРёСЃСЊ РµРіРѕ РІ char buff
+		for(int i(0); i < 50; i++){ //РїРѕСЃРёРјРІРѕР»СЊРЅС‹Р№ РїРµСЂРµРІРѕРґ РІ РЅРёР¶РЅРёР№ СЂРµРіРёСЃС‚СЂ
+
 			index = buff[i];
 			buff[i] = (char)tolower(index);
 		}
-		find(text,buff);//функция поиска(find.cpp)
+		find(text,buff);//С„СѓРЅРєС†РёСЏ РїРѕРёСЃРєР°(find.cpp)
 	}
 }
