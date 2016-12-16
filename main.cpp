51cf5e3d7225338e98fed6e9b64daac333ae377a
Mikhail Smorodin <zis.is.kiss@gmail.com>
2016-12-17 06:52:06 +0700

    Final version

--- main.cpp
+++ main.cpp
@@ -7,22 +7,16 @@ using namespace std;
 
 main(){    
 	char index;
-	char *buff = new char;
+	char buff[250];
 	while(1){
 		system("cls");
 		cout << "***Irregular Verbs***" << endl;
 		cout << "Enter the verb(English input, no more than 12 characters): ";
-		gets(buff);//ввод слова и запись его в char buff
-		if(strlen(buff) < 24){
-			for(int i(0); i < strlen(buff); i++){ //посимвольный перевод в нижний регистр
-				index = buff[i];
-				buff[i] = (char)tolower(index);
-			}
-			find(buff);//функция поиска(find.cpp)
-		}else{
-			cout << "too long" << endl;
-			system("pause");
+		cin.getline(buff, 250);//ввод слова и запись его в char buff
+		for(int i(0); i < strlen(buff); i++){ //посимвольный перевод в нижний регистр
+			index = buff[i];
+			buff[i] = (char)tolower(index);
 		}
-		delete buff;
+		find(buff);//функция поиска(find.cpp)
 	}
 }
