130906f7ee452ad09b6787af7ac225aac3c7b782
Mikhail Smorodin <zis.is.kiss@gmail.com>
2016-12-16 16:18:32 +0700

    long in

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@ using namespace std;
 
 main(){    
 	FILE *text;
-	char buff[50], index;
+	char buff[256], index;
 	while(1){
 		system("cls");
 		cout << "***Irregular Verbs***" << endl;
-		cout << "Enter the verb: ";
-		cin.getline(buff,50);//ввод слова и запись его в char buff
-		for(int i(0); i < 50; i++){ //посимвольный перевод в нижний регистр
-
+		cout << "Enter the verb(no more than 50 characters): ";
+		cin.getline(buff, 255);//ввод слова и запись его в char buff
+		for(int i(0); i < strlen(buff); i++){ //посимвольный перевод в нижний регистр
 			index = buff[i];
 			buff[i] = (char)tolower(index);
 		}
